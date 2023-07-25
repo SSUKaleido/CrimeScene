16760fc817b62cda9a9706f4e7fd4d483e41cc48
Jongeun <bubble2948@naver.com>
2023-07-25 21:25:22 +0900

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ApplicationQuit()
+    {
+        Application.Quit();
+    }
+}

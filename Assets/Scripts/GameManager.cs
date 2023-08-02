ce2df1a64a745748086d271a19be4f55eafcb886
Jongeun <bubble2948@naver.com>
2023-08-03 00:49:50 +0900

    Exit Toast Message
    만약 이 변경이 동작하지 않을 경우 Project Manager - (+) - Add Git Package에서
    
    https://github.com/NicholasSheehan/Unity-Native-Toasts.git#v1.0.4
    
    를 추가해보세요.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,18 +14,19 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
     }
 
-    void Update()
-    {
-        
-    }
-
     private bool ExitContinuityCheck = false;
+    private string toastMessage = "\'취소\' 명령을 한 번 더 입력하면 종료합니다";
 
     public IEnumerator ApplicationQuit()
     {
         if (ExitContinuityCheck == false)
         {
             ExitContinuityCheck = true;
+            #if UNITY_EDITOR
+                Debug.Log(toastMessage);
+            #else
+                ToastMessageHelper.ShowToastMessage(toastMessage);
+            #endif
             yield return new WaitForSeconds(1.5f);
             ExitContinuityCheck = false;
         }
---
+++ Assets/Scripts/ToastMessageHelper.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityNative.Toasts;
+
+public class ToastMessageHelper : MonoBehaviour
+{
+    private static readonly IUnityNativeToasts unityNativeToasts;
+
+    static ToastMessageHelper() {
+        unityNativeToasts = UnityNativeToasts.Create();
+    }
+
+    public static void ShowToastMessage(string toastText) {
+        unityNativeToasts.ShowLongToast(toastText);
+    }
+}
